1b0a1857f796127bd587e8c544bf36a72af43403
Corey Burns <coreymburns@gmail.com>
2022-10-24 11:16:42 -0400

    removed subs/queries that arent working with updated deps, upgraded apollo server 3 --> 4

--- src/config/initializeExpress.ts
+++ src/config/initializeExpress.ts
@@ -1,25 +1,25 @@
-import cors from 'cors';
-import 'dotenv-safe/config';
-import express from 'express';
-import session from 'express-session';
-import 'reflect-metadata';
-import { COOKIE_NAME } from '../common/constants';
-import initializeRedis from './redisConfig';
+import cors from 'cors'
+import 'dotenv-safe/config'
+import express from 'express'
+import session from 'express-session'
+import 'reflect-metadata'
+import { COOKIE_NAME } from '../common/constants'
+import initializeRedis from './redisConfig'
 
-const APOLLO_STUDIO_URL = 'https://studio.apollographql.com';
+const APOLLO_STUDIO_URL = 'https://studio.apollographql.com'
 
 export default function initializeExpress() {
-  const { redisStore, redisClient } = initializeRedis();
+  const { redisStore, redisClient } = initializeRedis()
 
-  const app = express();
+  const app = express()
 
-  app.set('trust proxy', process.env.NODE_ENV !== 'production');
+  app.set('trust proxy', process.env.NODE_ENV !== 'production')
   app.use(
     cors({
-      origin: 'https://studio.apollographql.com',
+      origin: ['https://studio.apollographql.com', 'http://localhost:3000'],
       credentials: true,
     }),
-  );
+  )
   app.use(
     session({
       name: COOKIE_NAME,
@@ -29,16 +29,16 @@ export default function initializeExpress() {
       cookie: {
         httpOnly: true,
         maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
-        // sameSite: 'lax',
-        // secure: 'auto',
-        sameSite: 'none',
-        secure: true,
+        sameSite: 'lax',
+        secure: 'auto',
+        // sameSite: 'none',
+        // secure: true,
       },
       saveUninitialized: false,
       secret: process.env.SESSION_SECRET,
       resave: false,
     }),
-  );
+  )
 
-  return { app };
+  return { app }
 }
