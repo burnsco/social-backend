cb544fba2b0e92aede67dc375287426d26d14778
Corey Burns <coreymburns@gmail.com>
2022-10-26 11:15:58 -0400

    fixed a few error messages, user forgot/change password works perfectly now

--- src/resolvers/Category/Mutation.ts
+++ src/resolvers/Category/Mutation.ts
@@ -1,12 +1,12 @@
-import { LoadStrategy } from '@mikro-orm/core';
-import { Arg, Ctx, Mutation, Resolver, UseMiddleware } from 'type-graphql';
-import { subRedditNameInUse } from '../../common/constants';
-import { Category, User } from '../../entities';
-import { CategoryInput } from '../../inputs';
-import { isAuth } from '../../lib/isAuth';
-import { CategoryMutationResponse } from '../../responses';
-import UserLeaveJoinSubResponse from '../../responses/subscription/user-leave-join-sub-response';
-import { ContextType } from '../../types';
+import { LoadStrategy } from '@mikro-orm/core'
+import { Arg, Ctx, Mutation, Resolver, UseMiddleware } from 'type-graphql'
+import { categoryNameInUse } from '../../common/constants'
+import { Category, User } from '../../entities'
+import { CategoryInput } from '../../inputs'
+import { isAuth } from '../../lib/isAuth'
+import { CategoryMutationResponse } from '../../responses'
+import UserLeaveJoinSubResponse from '../../responses/subscription/user-leave-join-sub-response'
+import { ContextType } from '../../types'
 
 @Resolver(() => Category)
 export default class CategoryMutationResolver {
@@ -16,20 +16,20 @@ export default class CategoryMutationResolver {
     @Arg('data') data: CategoryInput,
     @Ctx() { em }: ContextType,
   ): Promise<CategoryMutationResponse> {
-    const isNameInUse = await em.findOne(Category, { name: data.name });
+    const isNameInUse = await em.findOne(Category, { name: data.name })
     if (!isNameInUse) {
       const category = em.create(Category, {
         createdAt: new Date(),
         updatedAt: new Date(),
         name: data.name,
-      });
-      await em.persistAndFlush(category);
+      })
+      await em.persistAndFlush(category)
 
-      return { category };
+      return { category }
     }
     return {
-      errors: [subRedditNameInUse],
-    };
+      errors: [categoryNameInUse],
+    }
   }
 
   @Mutation(() => UserLeaveJoinSubResponse)
@@ -42,24 +42,24 @@ export default class CategoryMutationResolver {
       User,
       { id: req.session.userId },
       { populate: ['chatRooms'], strategy: LoadStrategy.JOINED },
-    );
+    )
     const category = await em.findOne(
       Category,
       { name: data.name },
       { populate: ['chatUsers'], strategy: LoadStrategy.JOINED },
-    );
+    )
 
     if (category && user && category.chatUsers.contains(user)) {
-      return null;
+      return null
     }
 
     if (category && user) {
-      category.chatUsers.add(user);
-      user.chatRooms.add(category);
-      await em.flush();
-      return { category, user };
+      category.chatUsers.add(user)
+      user.chatRooms.add(category)
+      await em.flush()
+      return { category, user }
     }
-    return null;
+    return null
   }
 
   @Mutation(() => UserLeaveJoinSubResponse)
@@ -72,23 +72,23 @@ export default class CategoryMutationResolver {
       User,
       { id: req.session.userId },
       { populate: ['chatRooms'], strategy: LoadStrategy.JOINED },
-    );
+    )
     const category = await em.findOne(
       Category,
       { name: data.name },
       { populate: ['chatUsers'], strategy: LoadStrategy.JOINED },
-    );
+    )
     if (
       category &&
       user &&
       category.chatUsers.contains(user) &&
       user.chatRooms.contains(category)
     ) {
-      category.chatUsers.remove(user);
-      user.chatRooms.remove(category);
-      await em.flush();
-      return { category, user };
+      category.chatUsers.remove(user)
+      user.chatRooms.remove(category)
+      await em.flush()
+      return { category, user }
     }
-    return null;
+    return null
   }
 }
